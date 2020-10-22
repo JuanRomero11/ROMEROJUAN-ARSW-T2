4e919d6ef983b10c6addc8562a46f5ffb02c12d3
Juan Guillermo Romero <juan.romero-si@escuelaing.edu.co>
2020-10-22 12:48:58 -0500

    back construido(modelo) y primer servicio contruido sin probar

---
+++ src/main/java/edu/eci/arsw/weather/services/WeatherStatsService.java
@@ -0,0 +1,9 @@
+package edu.eci.arsw.weather.services;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import edu.eci.arsw.weather.model.*;
+
+
+public interface WeatherStatsService {
+    City getStatsByCity(String name) throws UnirestException;
+}
